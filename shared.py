baec5dddef52bd6f98793e8bdcc1be41ffac5b57
Carlos Andrés Planchón Prestes <carlosandresplanchonprestes@gmail.com>
2025-04-05 21:04:47 -0300

    Adding Gemini 2.5 Pro Experimental to shared.py

--- shared.py
+++ shared.py
@@ -2,6 +2,10 @@
 
 from langchain.chat_models import init_chat_model
 
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# OpenAI:
+# https://platform.openai.com/docs/api-reference/introduction
 gpt4o_llm = init_chat_model(
     model="gpt-4o",
     model_provider="openai"
@@ -13,4 +17,11 @@ gpt45_llm = init_chat_model(
 o1_llm = init_chat_model(
     model="o1",
     model_provider="openai"
+)
+
+# Google Gemini:
+# https://ai.google.dev/gemini-api/docs/models#gemini-2.5-pro-preview-03-25
+gemini_2_5_llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-pro-exp-03-25",
+    temperature=0,
 )
